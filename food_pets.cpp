61105ce0f5655507a1d5b41dd6a669affcafe7ea
JRAMAHE'S <juanramhdiconce@gmail.com>
2023-02-22 22:51:47 -0800

    Create food_pets.cpp

---
+++ food_pets.cpp
@@ -0,0 +1,12 @@
+// creating the title
+#include <iostream > 
+#include <vector > 
+
+void Array_func()
+{
+  std::vector<float>vec{10.23};   
+
+int main ()
+{
+  std::cout << "this is the new title for the sushi place " << std::endl;
+}
